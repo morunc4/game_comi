84f380ca230c1df7263ce5bd7de1ae392a99e14e
morunc4 <k7f2bd2d72k7uf5@gmail.com>
2022-11-12 11:34:38 +0000

    key変更

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,21 +1,21 @@
 class Public::CommentsController < ApplicationController
   def create
-    @review=Review.find(params[:review_id])
-    @comment=Comment.new(comment_params)
-    @comment.user_id=current_user.id
-    @comment.review_id=@review.id
+    @review = Review.find(params[:review_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.review_id = @review.id
     if @comment.save
       redirect_to rakuten_game_review_path(@review.rakuten_game_id,@review.id)
     else
-      @comments=@review.comments.all
+      @comments = @review.comments.all
       @rakuten_game = RakutenGame.find(params[:rakuten_game_id])
       render template: "public/reviews/show"
     end
   end
 
   def destroy
-    @review=Review.find(params[:review_id])
-    @comment=Comment.find(params[:id])
+    @review = Review.find(params[:review_id])
+    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to rakuten_game_review_path(@review.rakuten_game_id,@review.id)
   end
